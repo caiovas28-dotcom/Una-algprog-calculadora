1502e541f5f97aed5cbfd39ccda514a1144c7954
caiovas <caiovas28@gmail.com>
2026-02-27 21:09:58 -0300

    Add files via upload

---
+++ Calculadora/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Calculadora");
+
+Console.WriteLine("Digite o primeiro número:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo  número:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Escolha a operação:");
+Console.WriteLine("1 - adição");
+Console.WriteLine("2 - subtração");
+Console.WriteLine("3 - Multiplicação");
+Console.WriteLine("4 - Divisão");
+int operação = Convert.ToInt32(Console.ReadLine());
+
+switch (operação)
+{
+    case 1:
+    Console.WriteLine($"Resultado: {num1 + num2}");
+    break;
+
+    case 2:
+
+     Console.WriteLine($"Resultado: {num1 -num2}");
+    break;
+
+     case 3:
+
+     Console.WriteLine($"Resultado: {num1 *num2}");
+    break;
+
+
+
+     case 4:
+        if (num2 != 0)
+            Console.WriteLine($"Resultado: {num1 / num2}");
+        else
+            Console.WriteLine("Erro: Divisão por zero não é permitida.");
+        break;
+    default:
+        Console.WriteLine("Operação inválida.");
+        break;
+
+}
+
+
+
+
